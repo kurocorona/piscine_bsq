e05f593111267c0e5d02225dc61c99c98ea5032c
tkomatsu <komatsu0315@icloud.com>
2020-01-28 12:47:31 +0900

    構造体を修正しました。 add sq_point_s, sq_point_l, sq_bool, err

--- includes/ft_tools.h
+++ includes/ft_tools.h
@@ -6,22 +6,23 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/uio.h>
+typedef struct s_map
+{
+	int		line_count;
+	char	*mark;
+	char	**map;
+	int		sq_point_s[2];
+	int		sq_point_l[2];
+	int		sp_bool;
+	int		err;
+}			t_map;
 
 void	ft_putchar(char c);
 void	ft_putstr(char *str);
 int		ft_strlen(char *str);
-int ft_putmap(t_map *ans);
-
-typedef struct s_map
-{  
-    int     line_count;  
-    char    *mark; 
-    char    **map;  
-}			t_map;
-
+int		ft_count_line(char *filename);
+int		ft_putmap(t_map *ans);
 t_map	*ft_init_map(char *filename);
-int	    ft_count_line(char *filename);
 t_map	*ft_insert_val(int fd, int *j, char *filename, int *i);
-
 
 #endif
